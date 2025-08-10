5afeadda1ee107c22b5ad4f5a129145c080dfd76
learnandsail <fcs090sutaromkar@gmail.com>
2025-08-10 17:39:18 +0530

    secondchange

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const schoolController = require('../controllers/schoolController');
 
-router.post('/addSchool', schoolController.addSchool);
+router.post('/addschool', schoolController.addSchool);
 
 router.get('/listSchools', schoolController.listSchools);
 
